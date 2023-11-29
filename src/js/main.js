a50e7c098a0409b20f7b754decd07ff8c1381b42
Andrii <borysenko22@gmail.com>
2023-11-29 21:18:24 +0100

    Create new prototype sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-import Slider from './modules/slider';
+import MainSlider from './modules/slider/slider-main';
 import VideoPlaer from './modules/playVideo';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new Slider('.page', '.next');
+    const slider = new MainSlider({btns: '.page', page:'.next'});
     slider.render();
 
     const player = new VideoPlaer('.showup .play', '.overlay');
